2bd6e05866b91988ee28e3da45a16b888523b46a
FATTY-GLUTTON\POCHI <helloworld.zip@gmail.com>
2019-11-07 05:50:30 +0900

    compatible with Windows (MSYS2)

--- build.rs
+++ build.rs
@@ -3,10 +3,23 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let _dst = Config::new("libpd").build_target("libpd_static").build();
+    if cfg!(target_os = "windows") {
+       Config::new("libpd")
+            .build_target("libpd_static")
+            .define("CMAKE_LIBRARY_PATH", "C:/msys64/mingw64/x86_64-w64-mingw32/lib")
+            .build();
+    } else {
+        Config::new("libpd")
+            .build_target("libpd_static")
+            .build();
+    }
 
     println!("cargo:rustc-link-search=native=libpd/libs");
-    println!("cargo:rustc-link-lib=static=pd");
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=static=libpd-static");
+    } else {
+        println!("cargo:rustc-link-lib=static=pd");
+    }
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
